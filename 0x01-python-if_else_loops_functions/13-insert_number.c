d7b9aef198905c867a0d5e80f32919583629e815
Laure Tchuente <solaure2t@yahoo.fr>
2023-03-07 15:49:49 -0800

    task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * insert_node - insert a node in a listint_t list
+ * Description: insert a node in a right place in a list
+ * @head: pointer to head of list
+ * @number: number to insert
+ * Return: address of the node insert
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *res;
+	listint_t *h, *tmp;
+
+	tmp = *head;
+	if (tmp == NULL)
+		return (NULL);
+	res = malloc(sizeof(listint_t));
+	if (res == NULL)
+		return (NULL);
+	res->n = number;
+	while (tmp != NULL && tmp->n <= number)
+	{
+		h = tmp;
+		tmp = tmp->next;
+	}
+	h->next = res;
+	if (tmp != NULL)
+		res->next = tmp;
+	else
+		res->next = NULL;
+	return (res);
+}
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,8 @@
 def uppercase(str):
     for i in range(0, len(str)):
         if str[i] > 'a' and str[i] < 'z':
-            if i == len(str) - 1:
-                print("{}".format(chr(ord(str[i]) - 32)))
-            else:
-                print("{}".format(chr(ord(str[i]) - 32)), end="")
+            ch = chr(ord(str[i]) - 32)
         else:
-            continue
+            ch = str[i]
+        print("{}".format(ch), end="")
+    print()
