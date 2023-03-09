198ba972cd9bd635bdb994de16cfbf173bdc0f9d
Laure Tchuente <solaure2t@yahoo.fr>
2023-03-09 15:35:28 -0800

    task 6

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) <= 1:
-        print("0 argument.")
+        print("0 arguments.")
     else:
         if len(sys.argv) == 2:
             print("1 argument:")
         else:
-            print("{} arguments".format(len(sys.argv)))
+            print("{} arguments".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
