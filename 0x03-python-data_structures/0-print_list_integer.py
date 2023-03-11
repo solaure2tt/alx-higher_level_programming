3e133366190256c59449d8551f9cf24cc7cb13d5
Laure Tchuente <solaure2t@yahoo.fr>
2023-03-11 09:27:37 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     if len(my_list) > 0:
         for i in range(0, len(my_list)):
-            print("{}".format(my_list[i]))
+            print("{:d}".format(my_list[i]))
