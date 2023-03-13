de2af6a38b594c7ec492b2bd2c2c848a2a94ec8f
Laure Tchuente <solaure2t@yahoo.fr>
2023-03-13 12:27:35 -0700

    task 7 update 1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     res = []
-    i = 0
-    for x in tuple_a:
-        i = i + 1
-        if i > 2:
-            break
-        if x:
-            res.append(x)
-        else:
+    res2 = []
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            res. append(0)
             res.append(0)
-    i = 0
-    for y in tuple_b:
-        i + i + 1
-        if i > 2:
-            break
-        if y:
-            res[i] = res[i] + y
-        i = i + 1
-    return tuple(res)
+        else:
+            res.append(tuple_a[0])
+            res.append(0)
+    else:
+        res.append(tuple_a[0])
+        res.append(tuple_a[1])
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            res2. append(0)
+            res2.append(0)
+        else:
+            res2.append(tuple_a[0])
+            res2.append(0)
+    else:
+        res2.append(tuple_b[0])
+        res2.append(tuple_b[1])
+    return (res[0] + res2[0], res[1] + res2[1])
