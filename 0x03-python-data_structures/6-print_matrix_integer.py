e895f0ac05c807a9b88ddb84a40a02a48ebc7e2e
Laure Tchuente <solaure2t@yahoo.fr>
2023-03-12 19:08:48 -0700

    task 6 update ckeck6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) > 0:
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print("")
+    else:
         for inner_list in matrix:
             for i in range(0, len(inner_list)):
                 if i == len(inner_list) - 1:
                     print("{:d}".format(inner_list[i]))
                 else:
                     print("{:d}".format(inner_list[i]), end=" ")
-    else:
-        print("")
