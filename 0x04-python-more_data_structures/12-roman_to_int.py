56beac027fb6302ae8df3120ea6886ade2e64d44
Laure Tchuente <solaure2t@yahoo.fr>
2023-03-15 19:33:40 -0700

    task 12 update 1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,9 @@ def roman_to_int(roman_string):
     ro = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     res = 0
     x = len(roman_string)
+    for id in range(len(roman_string)):
+        if ro.get(roman_string[id], 0) == 0:
+            return (0)
     for (i, r) in enumerate(roman_string):
         if i < x - 1 and ro[r] < ro[roman_string[i + 1]]:
             res -= ro[r]
