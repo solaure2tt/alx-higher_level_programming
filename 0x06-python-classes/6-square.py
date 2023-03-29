0f5a0609226fb27594e791b2839ac6d354bbb66c
Laure Tchuente <solaure2t@yahoo.fr>
2023-03-29 08:28:25 -0700

    task 6 update chexks 15, 16

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,8 +39,11 @@ class Square:
             if self.__size == 0:
                 print()
             else:
+                if self.__position[1] > 0:
+                    for li in range(self.__position[1]):
+                        print()
                 for i in range(self.__size):
-                    if self.__position[0] >= 0:
+                    if self.__position[0] > 0:
                         for li in range(self.__position[0]):
                             print(" ", end="")
                     for j in range(self.__size):
