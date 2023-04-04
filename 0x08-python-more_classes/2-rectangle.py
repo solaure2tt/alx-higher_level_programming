d33c89ef1d32f8f292033389253ec200de4d3446
Laure Tchuente <solaure2t@yahoo.fr>
2023-04-03 17:28:36 -0700

    task 2 update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,13 +4,56 @@
 
 class Rectangle:
     """This class is to defined a rectangle"""
+
     def __init__(self, width=0, height=0):
         """instanciation of a rectangle
-        Args: width - the width of the rectangle
+        Args: 
+            width - the width of the rectangle
             height - the height of the rectangle
-            """
-        self.__width = width
-        self.__height = height
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieve a width of the rectangle"""
+        if self.__width:
+            return self.__width
+        else:
+            return 0
+
+    @width.setter
+    def width(self, value):
+        """set the value of the width
+        Args: value - value of the width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve the value of the height"""
+        if self.__height:
+            return self.__height
+        else:
+            return 0
+
+    @height.setter
+    def height(self, value):
+        """set the value of the height
+        Args: value - value of the height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """to calculate the area"""
@@ -21,33 +64,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return ((self.__width + self.__height) * 2)
-
-    @property
-    def width(self):
-        """retrieve the with of the rectangle"""
-        return self.width
-
-    @width.setter
-    def width(self, value):
-        """set the with of the rectangle
-        Args: value - value of the width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """retrieve a height"""
-        return self.height
-
-    @height.setter
-    def height(self, value):
-        """set the height of the rectangle
-        Args: value - value of the height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
