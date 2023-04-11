eb4d8c2381a32d035084b30c24e2d045044da28e
Laure Tchuente <solaure2t@yahoo.fr>
2023-04-11 12:16:18 -0700

    task 12 update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""This module is to class MyInt that inherits
-from int
-"""
+"""Defined a class MyInt"""
 
 
 class MyInt(int):
-    """create a class tha inherit from int"""
+    """create a class that inherit from int"""
 
     def __eq__(self, value):
-        """implement the egality for MyInt
-        Args: value - first int
-        """
+        """implement the egality for MyInt"""
         return self.real != value
 
     def __ne__(self, value):
-        """implement the difference operator for MyInt
-        Args: value - first int
-        """
+        """implement the difference operator for MyInt"""
         return self.real == value
