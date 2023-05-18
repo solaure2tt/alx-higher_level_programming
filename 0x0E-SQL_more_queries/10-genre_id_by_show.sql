5f8a8c3e921940d01d5406e30111c4ae2b5994a0
Laure Tchuente <solaure2t@yahoo.fr>
2023-05-17 17:41:05 -0700

    task 10 update

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,8 @@
 --    Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
-SELECT tvs.title, tvg.genre_id
-FROM tv_shows as tvs INNER JOIN tv_show_genres as tvg
-    ON tvs.id = tvg.show_id
-ORDER BY tvs.title, tvg.genre_id ASC
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
