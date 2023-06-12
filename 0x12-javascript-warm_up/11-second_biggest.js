515e98dbb8c939e6dce8e6702a4b3abeca96915d
solaure2tt <solaure2t@yahoo.fr>
2023-06-12 19:48:21 +0000

    task 11 update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,34 +2,34 @@
 const process = require('process');
 const argv = process.argv;
 if (argv.length < 3) {
-  console.log('0')
+  console.log('0');
 } else if (argv.length === 3) {
-  console.log('0')
+  console.log('0');
 } else {
-    let max = parseInt(argv[2]);
-    let i = 3;
-    let sec;
+  let max = parseInt(argv[2]);
+  let i = 2;
+  let sec;
+  argv.forEach((val, index) => {
+    if (index > 2) {
+      if (max < parseInt(val)) {
+        max = parseInt(val);
+      }
+    }
+  })
+  while ((i < argv.length) && (max < parseInt(argv[i]))) {
+    i = i + 1;
+  }
+  if (i < argv.length) {
+    sec = parseInt(argv[i]);
     argv.forEach((val, index) => {
-        if (index > 2) {
-          if (max < parseInt(val)) {
-            max = parseInt(val);
-          }
+      if (index > 1) {
+        if ((sec <  parseInt(val)) && (parseInt(val) < max)) {
+          sec = parseInt(val);
         }
-      })
-    while (i < argv.length && max < parseInt(argv[i])) {
-      i = i + 1;
       }
-    if (i < argv.length) {
-      sec = parseInt(argv[i]);
-      argv.forEach((val, index) => {
-        if (index > 1) {
-          if (sec <  parseInt(val) && parseInt(val) < max) {
-            sec = parseInt(val);
-          }
-        }
-      })
-    } else {
-        sec = 0
-      }
+    })
+  } else {
+    sec = 0;
+    }
     console.log(sec);
 }
