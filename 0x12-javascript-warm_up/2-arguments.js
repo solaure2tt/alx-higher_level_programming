2d8eb3b46c08e3d9486ed092db3e58f0b3d1f9f4
solaure2tt <solaure2t@yahoo.fr>
2023-06-12 19:17:32 +0000

    updt 2 task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const process = require('process');
 const argv = process.argv;
 if (argv.length < 3) {
-  console.log('No argument')
+  console.log('No argument');
 } else if (argv.length === 3) {
-    console.log('Argument found')
+    console.log('Argument found');
 } else {
-    console.log('Arguments found')
+    console.log('Arguments found');
 }
