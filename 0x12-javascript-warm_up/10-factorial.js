381a834ab83c9fd1984aa0c1674e540ba0d4ba8b
solaure2tt <solaure2t@yahoo.fr>
2023-06-12 19:37:47 +0000

    upd1 task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const argv = process.argv;
 function fact(a) {
-  if (a === 1 || a === 0) {
+  if ((a === 1) || (a === 0)) {
     return 1;
   }
   return (a * fact(a - 1));
