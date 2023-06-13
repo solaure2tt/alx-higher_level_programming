3df5cebf56cd02c1f2b44e8f5c3befd6f8a89c8a
solaure2tt <solaure2t@yahoo.fr>
2023-06-13 19:19:30 +0000

    upd task 6, 9 10

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,39 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const SquareParent = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let line = '';
-      for (let j = 0; j < this.width; j++) {
-        line = line + 'X';
-      }
-      console.log(line);
-    }
-  }
-
-  rotate () {
-    const x = this.width;
-    this.width = this.height;
-    this.height = x;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends SquareParent {
   charPrint (c) {
     if (typeof c === 'undefined') {
       this.print();
@@ -48,5 +16,4 @@ class Square extends Rectangle {
     }
   }
 }
-module.exports = Rectangle;
 module.exports = Square;
