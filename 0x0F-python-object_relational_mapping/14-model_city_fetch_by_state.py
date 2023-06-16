589ebadd280dbc3dc4bf593cc85f4c8b14d799cb
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-16 10:49:02 -0700

    task 6 to 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all cities  objects from a given database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(State, City).filter(City.state_id == State.id)
+    for state, city in cities.order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that lists the first State objects from a given database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).order_by(State.id).first()
+    if instance is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(instance.id, instance.name))
