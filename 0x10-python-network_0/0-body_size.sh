22275a4dcc37ab1cda8c0e459ae2f51cb4f46195
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-30 12:03:43 -0700

    upddate task 4

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
 curl -si "$1" | sed -n 's/Content-Length: \([0-9]\+\)/\1/p'
-
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# script that sends a JSON POST request to a URL passed as the
-# first argument, and displays the body of the response.
-# Your script must send a POST request with the contents of a file,
-# passed with the filename as the second argument of the script, in the body of the request
+# script that sends a JSON POST request to a UR
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
