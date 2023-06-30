101d718cfed81da88353124f4b07f71272f5d3f5
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-30 12:56:49 -0700

    task 0 to 2

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == '__main__':
+    add = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(add) as response:
+        res = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))
