928d3c2ebccd72971f9ba4b34c229776a1d1ed05
solaure2tt <solaure2t@yahoo.fr>
2023-08-02 21:02:01 -0700

    task 0 upd

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-var fs = require('fs');
-var file = process.argv[2];
+let fs = require('fs');
+let file = process.argv[2];
 fs.readFile(file, 'utf-8', (error, text) => {
   if (error) {
     console.log(error);
